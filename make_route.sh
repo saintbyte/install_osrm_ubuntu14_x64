c025371d846338a30b0c6f3de75a01c592584908
root <root@azurefirst.offlinemaps.f2.internal.cloudapp.net>
2014-10-11 17:45:57 +0000

    Count cores

--- make_route.sh
+++ make_route.sh
@@ -3,6 +3,7 @@ set +ue
 set -x
 source ./vars.sh
 OSRM_DIR="${DISK}/osrm_tmp/"
+CORES=`nproc`
 mkdir "${OSRM_DIR}"
 #$PLANET_OSM_PATH
 echo "move route"
@@ -16,4 +17,4 @@ touch ${OSRM_DIR}.stxxl
 echo "disk=${DISK}/osrm_tmp_file,36000,syscall" > ${OSRM_DIR}.stxxl
 echo "Route make"
 cd ${OSRM_DIR}
-nice -n -10 ${OSRM_DIR}osrm-extract -p ${OSRM_DIR}/profile.lua -t 2 $PLANET_OSM_PATH 2>${OSRM_DIR}output 3>${OSRM_DIR}err
+nice -n -10 ${OSRM_DIR}osrm-extract -p ${OSRM_DIR}/profile.lua -t ${CORES} $PLANET_OSM_PATH 2>${OSRM_DIR}output 3>${OSRM_DIR}err
