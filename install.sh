8893b9fc7ee0b2e6ede3abd92f4e2164d580a457
root <root@azurefirst.offlinemaps.f2.internal.cloudapp.net>
2014-10-11 17:29:27 +0000

    Seems work

--- install.sh
+++ install.sh
@@ -2,9 +2,15 @@
 set +ue
 set -x
 echo "For Ubuntu 14.04+"
+apt-get update
 echo "1"
-#apt-get install git
-#git clone git@github.com:Project-OSRM/osrm-backend.git osrm-backend
+apt-get install git
+git clone https://github.com/Project-OSRM/osrm-backend.git osrm-backend
+touch /etc/sysctl.con
+echo "kernel.shmall=1152921504606846720" >> /etc/sysctl.conf
+echo "kernel.shmmax=18446744073709551615" >> /etc/sysctl.conf
+sysctl -p
+
 echo "2"
 apt-get -y install \
 build-essential git cmake pkg-config libprotoc-dev libprotobuf8 \
@@ -14,7 +20,7 @@ libzip-dev libboost-all-dev lua5.1 liblua5.1-0-dev \
 libluabind-dev libluajit-5.1-dev libtbb-dev
 echo "3"
 cd osrm-backend
-mkdir -p build
+mkdir  build
 cd build
 cmake ..
 make
--- make_route.sh
+++ make_route.sh
@@ -11,6 +11,9 @@ cp ./osrm-backend/build/osrm-extract ${OSRM_DIR}
 cp ./osrm-backend/build/osrm-prepare ${OSRM_DIR}
 cp ./osrm-backend/build/osrm-routed ${OSRM_DIR}
 cp ./osrm-backend/profiles/car.lua ${OSRM_DIR}/profile.lua
+
+touch ${OSRM_DIR}.stxxl
+echo "disk=${DISK}/osrm_tmp_file,36000,syscall" > ${OSRM_DIR}.stxxl
 echo "Route make"
 cd ${OSRM_DIR}
-nice -n -10 ${OSRM_DIR}osrm-extract -p ${OSRM_DIR}/profile.lua -t 2 $PLANET_OSM_PATH
+nice -n -10 ${OSRM_DIR}osrm-extract -p ${OSRM_DIR}/profile.lua -t 2 $PLANET_OSM_PATH 2>${OSRM_DIR}output 3>${OSRM_DIR}err
